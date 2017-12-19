dc2f62bfd3d8a2ce415b5607810e6594c5103ed5
Ernesto Ramiro <atomms@gmail.com>
2017-12-19 13:58:15 +0100

    publishing

--- sign up image/src/sample/Controller.java
+++ sign up image/src/sample/Controller.java
@@ -13,31 +13,7 @@ public class Controller {
 	@FXML
 	public BorderPane imagepane;
 
-//	@FXML
-//	public void initialize() {
-//
-//		// // load the image
-////		 Image image = new Image("file:nikhil-kumar-231465.jpg");
-//		//
-//		 // simple displays ImageView the image as is
-//		 ImageView img = new ImageView("file:nikhil-kumar-231465.jpg");
-////		 img.setImage(image);
-//		
-//
-////		ImageView img = new ImageView(
-////				"http://weknowyourdreams.com/images/forest/forest-13.jpg");
-//
-//		//
-//		imagepane.setCenter(img);
-//		// imagepane.getChildren().addAll(img);
-//		// imagepane.setStyle("-fx-background-color: #000000;");
-//
-//		//
-//		// Scene scene = new Scene(pane);
-//		// img.fitWidthProperty().bind(stage.widthProperty());
-//		// img.setPreserveRatio(true);
-//
-//	}
+
 
 	public void showSendProfile() {
 		try {
@@ -46,7 +22,7 @@ public class Controller {
 			loader.setLocation(Main.class.getResource("second.fxml"));
 			AnchorPane page = (AnchorPane) loader.load();
 			Stage sendStage = new Stage();
-			sendStage.setTitle("Superheader");
+			sendStage.setTitle("FreeSolo");			
 			sendStage.show();
 
 		} catch (IOException e) {
